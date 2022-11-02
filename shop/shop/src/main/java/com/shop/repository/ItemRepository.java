4df44c081aba77d00c925bc4e6827ecbaaf52e10
syoung <dividividid@gmail.com>
2022-11-02 21:34:17 +0900

    스프링 시큐리티 설정

--- shop/shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -2,7 +2,33 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    //price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+
+    //쿼리로 서치
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+
+    //복잡한 쿼리를 사용할 경우에는 네이티브 쿼리를 사용하도록 한다
+    @Query(value="select *from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+
+
 
 }
