be6a5a66a7e667f6fe9600832f93d2b546744ec6
bbangdi <bbangord@gmail.com>
2022-11-03 23:02:15 +0900

    시큐리티 설정 추가

--- shop/shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.shop.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//WebSecurityConfigurerAdapter : http 요청에 대한 보안설정
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
